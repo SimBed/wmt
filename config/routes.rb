4c86479b15219615d732b2d4d61ba148b77657cf
SimBed <dansimbed@gmail.com>
2026-07-03 09:21:00 +0100

    add route to ping to keep awake on homeserver

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   resources :passwords, param: :token
 
   get "up" => "rails/health#show", as: :rails_health_check
+  # route to ping to keep DB connections alive on homeserver
+  get "/health", to: proc { ActiveRecord::Base.connection.execute("SELECT 1")
+                            [ 200, {}, [ "OK" ] ]
+                          }, as: :ping_to_keep_app_awake  
 end
